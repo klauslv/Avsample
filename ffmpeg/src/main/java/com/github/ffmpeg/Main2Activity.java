5b038ee291aac0623049885e62337252ac6dedca
lvming <lvming@guazi.com>
2020-12-24 14:39:55 +0800

    ffmpeg test

---
+++ ffmpeg/src/main/java/com/github/ffmpeg/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.github.ffmpeg;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("ffmpeg_lib");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.sample_text);
+        textView.setText(getFFmpegVersion());
+    }
+
+    /**
+     * @return 返回当前
+     */
+    public native static String getFFmpegVersion();
+}
